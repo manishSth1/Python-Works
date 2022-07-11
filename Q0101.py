6db52b83a9c48185155424aad7d3733a99b9a5fb
manishSth1 <94635357+manishSth1@users.noreply.github.com>
2022-07-11 14:37:28 -0400

    Add files via upload
    "added solutions to Q0101"

---
+++ Q0101.py
@@ -0,0 +1,21 @@
+# Q0101 solutions
+
+# 1
+print('Manish Shrestha')
+
+# 2
+print(6 + 6)
+
+# 3
+print(5 + 10 + 15)
+
+# 4
+print(Hello World)
+
+
+# 5
+"""
+Since the strings needs to be quoted with single or double
+quotation marks, the above line gives error when the code
+is uncommented
+"""
