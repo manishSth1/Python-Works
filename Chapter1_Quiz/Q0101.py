2212ee5c86ffa8b97e5f6ab9d87410c142e15d78
manishSth1 <manishstha40@gmai.com>
2022-07-11 15:05:39 -0400

    solutions to chapter1 quizes

---
+++ Chapter1_Quiz/Q0101.py
@@ -0,0 +1,20 @@
+# Q0101 solutions
+
+# 1
+print('Manish Shrestha')
+
+# 2
+print(6 + 6)
+
+# 3
+print(5 + 10 + 15)
+
+# 4
+print(Hello World)
+
+
+# 5
+"""
+line 13
+SyntaxError: invalid syntax
+"""
