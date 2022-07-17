58e3175c95d19bac8c1c976c2af2418cdbc5ef47
manishSth1 <manishstha40@gmai.com>
2022-07-17 10:00:53 -0400

    added solutions to Q0203

--- Chapter2_Quiz/Q0203.py
+++ Chapter2_Quiz/Q0203.py
@@ -1,15 +1,15 @@
 first_name = "Manish"
 last_name = "Shrestha"
 
-# 1 with concatentation
+# 1 with + operator
 full_name = "My name is " + first_name + " " + last_name
 print(full_name)
 
-# 2 with format
+# 2 with format() method
 print("My name is {} {}".format(first_name, last_name))
 
-# 3 with f string
+# 3 with f-string
 print(f"My name is {first_name} {last_name}")
 
-# 4 
+# 4 %s method
 print("My name is %s %s" %(first_name, last_name))
