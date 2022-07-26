a0adfacb67803ed08f9d28899e379b3957fc2620
manishSth1 <manishstha40@gmai.com>
2022-07-25 23:15:31 -0400

    class works

--- class_works/works.py
+++ class_works/works.py
@@ -126,3 +126,108 @@ print(x.index(66))    # to find out index = list.index(list)element)
 # counting the number of items
 x = [25,2,5,98,66,100,25,4,0,1,25,98,4,5,3,25]
 print(x.count(25))  # to count items in list = list.count(list_element)
+
+# Tuples
+# tuples are immutble sequence (values cannot be changed like that in list)
+# represented with comma separated values enclosed by parentheses
+# tuples are faster than list
+
+tup_1 = () # empty tuple
+print(type(x))
+
+tup_2 = (1,2,3,4)   # same data type
+print(tup_2)
+
+tup_3 = (1, 2, "John", 4.5, True)   # different data type
+print(tup_3)
+tup_3    # in Jupyter Notebook, no need to use print function, can work
+
+list_1 = [1, 2, 4, 5,]     # can use comma at end of list and tuples as well
+tup_4 = tuple(list_1) 
+print(tup_4)
+
+tup = ('john')    # generally we put statement in (). Example - (5+4) * (7-5) = 9 * 2 = 18. They are not tuples, only grouped
+print(tup)
+type(tup)     # gives string
+
+tup_5 = (
+        'John'
+        'Doe'
+        )        # if used in () and not puta comma for, then it's not a tuple
+print(type(tup_5))
+
+
+# so put cooma to make a tuple
+tup = ('John',)
+print(tup)
+print(type(tup))
+
+t1: tuple = (1, 2, 3)   # type hinting, way to declare data types
+print(type(t1))
+
+y: bool = True
+print(type(y))
+
+tup_3 = (1, 2, "John", 4.5, True)
+print(tup_3[0])
+print(tup_3[-1])
+
+x = [-2,47,53,1,2,3,7,8]
+print(x[1:9:1])
+print(x[1:9:2])
+print(x[0:9:2])
+print(x[::2])
+print(x[1:9:3])
+print(x[-4:-1])
+
+odd = (1, 3, 6)     # can't use .appen() as in list
+odd_new = (7,)
+odd1 = odd + odd_new
+print(odd1)
+
+odd = (1,3,5,7,9,11,13,15)
+odd = list(odd)   # re-declare to list
+odd[-1] = 17    
+print(odd)
+odd = tuple(odd) # convert to tuple from list
+
+odd = (1,3,5,7,9,11,13,15)
+odd = tuple(list(odd[0:7]))
+print(odd)
+
+odd = (1,3,5,7,9,11,13,15)
+odd = tuple(list(odd[:-1]) + [17])
+print(odd)
+
+odd = (1,3,5,7,9,11,13,15)
+odd = odd[:-1] + (17,)
+print(odd)
+
+t1 = (1, 2, 3)
+t1 = t1 + (4, 5, 6)   # adding tuples
+print(t1)
+
+# unpacking
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = x + y
+print([x, y])   # two list in list
+print(*x, *y)   # removes []of x when *x, removes again []of y when *y
+print(*x, y)     # removes []of x when *x but prints []of y
+print([*x, *y])  # prints inside a list
+print([x + y])  # list in list
+print(z)        # single list
+
+#unpacking
+odd = (1,3,5,7,9,11,13,15)
+odd = (*odd[:-1], 17)   # removes last element(15) and place 17
+print(odd)
+
+x = (1,2,3,7,8,9)
+print(len(x))
+x = (*x[:3], 4,5,6, *x[3:])  # puts value in where we need to using unpacking
+print(x)
+
+x = (1,2,3,7,8,9,1,1,5)
+print(x.count(1))
+print(x.index(2))
