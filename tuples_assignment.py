6c9dbed65d26d448269caa431d0d4cfc0de0bce3
manishSth1 <manishstha40@gmai.com>
2022-07-27 11:39:17 -0400

    Assignemnt for Tuples

--- tuples_assignment.py
+++ tuples_assignment.py
@@ -1,4 +1,7 @@
 
+from array import array
+
+
 tuple_1 = (1, 'apple', 2, 'banana', 3, 1, 3, 'cat', 'cat', 2, 3.5, 1, 1, 5, 8.6, True, False, 7.9, 1)  # created a tuple with different data types
 
 #1 
@@ -16,3 +19,4 @@ print(tuple_1.index(1, 15))    # finding the index of element:'1' beyond index 1
 print(tuple_1.index('apple'))  # finding the index of element:'apple' using .index() func
 print(tuple_1.index(0))        # counts 'True' as 1 and 'False' as 0 when counting elements -> even though there is no 0, but False counts as 0
 #print(tuple_1.index(False))   # gives same result as above 
+
