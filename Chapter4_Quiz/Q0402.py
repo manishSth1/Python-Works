2333bd51ada144cdac0319d99d7c1ea698917708
manishSth1 <manishstha40@gmai.com>
2022-08-07 23:40:11 -0400

    Solution for Q0402

--- Chapter4_Quiz/Q0402.py
+++ Chapter4_Quiz/Q0402.py
@@ -1,6 +1,6 @@
-word = input("Enter a word to check for palindrome: ")
-word = word.lower()
-if word == word[::-1]:
+word = input("Enter a word to check for palindrome: ")           # asking for input from the user
+word = word.lower()                                              # change the letters to lowercase so that even a word contains uppercase letter, it would still check
+if word == word[::-1]:                                           # reverses the letters in the word and check if they are the same
     print(f"{word} is a palindrome.")
 else:
     print(f"{word} is not a palindrome.")
