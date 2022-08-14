d622d8775cf1d067a85635078d1b93370081dc64
manishSth1 <manishstha40@gmai.com>
2022-08-14 12:43:53 -0400

    Timer with miliseconds elapsed

---
+++ class_works/timer.py
@@ -0,0 +1,25 @@
+import time
+
+hour = 0
+while hour < 24:
+    minute = 0
+    while minute < 60:
+        second = 0
+        while second < 60:
+            time.sleep(0.001)
+            print(f'Time Elapsed: {hour:0>2}:{minute:0>2}:{second:0>2}', end = "\r")  # end = "\r"
+            second += 1
+        minute += 1
+    hour += 1
+
+hour = 0
+while hour < 24:
+    minute = 0
+    while minute < 60:
+        second = 0
+        while second < 60:
+            time.sleep(1)
+            print(f'Time Elapsed: {hour:0>2}:{minute:0>2}:{second:0>2}', end = "\r")  # end = "\r"
+            second += 1
+        minute += 1
+    hour += 1
