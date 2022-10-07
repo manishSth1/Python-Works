38879500b96348258991c3c89eef8fd44b19a455
manishSth1 <manishstha40@gmai.com>
2022-10-07 19:38:06 -0400

    Birthday Calculator

--- Pilot_Projects/Birthday_Calculator.py
+++ Pilot_Projects/Birthday_Calculator.py
@@ -1,4 +1,5 @@
 from datetime import date
+import random
 print("To find out your Birth year")
 age = int(input("Enter your age: "))
 today = date.today()
@@ -12,3 +13,11 @@ birthday = int(input("Enter your Birth year: "))
 today = date.today()
 age = today.year - birthday
 print(f"You are {age} years old.")
+
+#%%
+import random
+characters = "abcedfghijklABCEDFGHIKL0123456789@#$%&?"
+password = random.sample(characters, 8)
+password = (''.join(password))
+print(password)
+#%%
